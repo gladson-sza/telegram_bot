bb67ea4599a23f55d8c2788a5d913604f45f15e0
gladson-sza <gladsonsouza@live.com>
2022-12-11 20:13:38 -0400

    Add certfy

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ Original file is located at
 """
 import telebot
 import os
+from flask import Flask, request
 
 API_KEY = '5682851395:AAEe7D_j4mCabT2fsQpltiJuFOR75fSml_c'
+
+app = Flask(__name__)
 
 REQUEST_DEFAULT = 'default'
 REQUEST_CPF = 'cpf'
@@ -82,4 +85,20 @@ def echo_all(message):
         bot.reply_to(message, FINISH_LABEL)
 
 
-bot.polling()
+# FLASK SERVER
+
+@app.route('/' + API_KEY, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode('utf-8'))])
+    return 'Bot Online!', 200
+
+
+@app.route('/')
+def set_webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://18.216.97.113/' + API_KEY)
+    return 'Bot Online!', 200
+
+
+if __name__ == '__main__':
+    app.run(app.run(ssl_context=('cert.pem', 'key.pem'), host='0.0.0.0', port=int(os.environ.get('PORT', 8000))))
